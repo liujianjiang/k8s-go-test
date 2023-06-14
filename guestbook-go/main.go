1a86f0c71f387497b3f7eb0943ebe4b7adbe359d
liujianjiang <875394153@qq.com>
2023-06-14 19:48:17 +0800

    update

--- guestbook-go/main.go
+++ guestbook-go/main.go
@@ -1,8 +1,12 @@
-package main 
-import "fmt" 
+// geektutu.com
+// main.go
+package main
 
+import "github.com/gin-gonic/gin"
 
-func main(){
-    fmt.Println("hello world")
-}
-
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, Geektutu")
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:3000
