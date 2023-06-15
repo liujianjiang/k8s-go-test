709f564cff39e43271d77b283a85f6b56b541998
liujianjiang <875394153@qq.com>
2023-06-15 13:58:18 +0800

    update

--- guestbook-go/main.go
+++ guestbook-go/main.go
@@ -2,10 +2,39 @@
 // main.go
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+)
+
+var Rdb *redis.Client
+
+// 初始化连接
+func InitClient() (err error) {
+	Rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis-master:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err = Rdb.Ping().Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	r := gin.Default()
+
+	fmt.Println("初始化redis……")
+	err := InitClient()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, Geektutu")
 	})
